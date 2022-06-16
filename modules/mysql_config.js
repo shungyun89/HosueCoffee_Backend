96ab4622695d36787c27d1c1d0bfc6bc72da1315
shungyun89 <27332358asdf@gmail.com>
2022-06-16 23:29:14 +0800

    修改資料庫連結

--- modules/mysql_config.js
+++ modules/mysql_config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:'heroku_1982f07e8198e65',
-    user:'be1e1469335d3f',
-    password:'fc9d2019',
-    database:'heroku_1982f07e8198e65',
+    host:'us-cdbr-east-05.cleardb.net',
+    user:'ba21e33c5e4ea7',
+    password:'6c95ca35',
+    database:'heroku_af5e06537276fcc',
     connectionLimit:10,
     waitForConnections:true,
     dateStrings: true
